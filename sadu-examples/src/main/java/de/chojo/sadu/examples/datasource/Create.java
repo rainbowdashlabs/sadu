7f19f48c049b12d8f29973f492f2c7988fa35122
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-08-18 13:03:58 +0200

    Fix license

--- sadu-examples/src/main/java/de/chojo/sadu/examples/datasource/Create.java
+++ sadu-examples/src/main/java/de/chojo/sadu/examples/datasource/Create.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) 2022 RainbowDashLabs and Contributor
+ */
+
 package de.chojo.sadu.examples.datasource;
 
 import com.zaxxer.hikari.HikariDataSource;
--- sadu-examples/src/main/java/de/chojo/sadu/examples/querybuilder/QueryBuilderExample.java
+++ sadu-examples/src/main/java/de/chojo/sadu/examples/querybuilder/QueryBuilderExample.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) 2022 RainbowDashLabs and Contributor
+ */
+
 package de.chojo.sadu.examples.querybuilder;
 
 import de.chojo.sadu.base.QueryFactory;
