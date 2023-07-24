749c9d8456b90d8005f86f6aeb90fb7cec67d9a7
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2023-07-24 16:04:01 +0200

    Add module descriptions

--- sadu-core/build.gradle.kts
+++ sadu-core/build.gradle.kts
@@ -1,3 +1,5 @@
+description = "SADU core module, containing common logic between modules."
+
 dependencies {
     api("org.slf4j", "slf4j-api", "2.0.7")
     api("org.jetbrains", "annotations", "24.0.1")
--- sadu-datasource/build.gradle.kts
+++ sadu-datasource/build.gradle.kts
@@ -1,3 +1,5 @@
+description = "SADU module to create a hikaricp datasource"
+
 dependencies {
     api("com.zaxxer", "HikariCP", "5.0.1")
     api(project(":sadu-core"))
