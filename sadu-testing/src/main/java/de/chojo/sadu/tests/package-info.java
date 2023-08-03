8b680d8983eff123306c8b884335913713627231
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2023-08-03 20:41:07 +0200

    Add package info

---
+++ sadu-testing/src/main/java/de/chojo/sadu/tests/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The tests package contains utils to simply validate the integrity of sadus updater files using junit.
+ * <p>
+ * Use {@link de.chojo.sadu.tests.SaduTests#execute(int, de.chojo.sadu.databases.Database[])} to execute all required tests
+ */
+package de.chojo.sadu.tests;
