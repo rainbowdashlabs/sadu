73213ac668d478fd37998807eb694e68cfd436dd
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2023-08-03 20:46:29 +0200

    add license header

--- sadu-testing/src/main/java/de/chojo/sadu/tests/SaduTests.java
+++ sadu-testing/src/main/java/de/chojo/sadu/tests/SaduTests.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ *     Copyright (C) RainbowDashLabs and Contributor
+ */
+
 package de.chojo.sadu.tests;
 
 import de.chojo.sadu.databases.Database;
