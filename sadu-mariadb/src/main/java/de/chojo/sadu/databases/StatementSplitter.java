e9880aaf7628e0785deb812b5a89bfe48bcd5f71
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2023-11-21 23:15:56 +0100

    Cleanup code and fix delimiter statements contained in sql statement

--- sadu-mariadb/src/main/java/de/chojo/sadu/databases/StatementSplitter.java
+++ sadu-mariadb/src/main/java/de/chojo/sadu/databases/StatementSplitter.java
@@ -6,178 +6,122 @@
 
 package de.chojo.sadu.databases;
 
+import org.intellij.lang.annotations.RegExp;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Splits a string of SQL statements into an array of statements.
  * <p>
- *     This class is used to split a string of SQL statements into an array of statements.
- *     The delimiter is not case insensitive.
+ * This class is used to split a string of SQL statements into an array of statements.
+ * The delimiter is not case sensitive.
  * </p>
  */
 public class StatementSplitter {
 
     /**
-     * This is the default regex, which is regularly used to split statements.
+     * The default marker, which is regularly used to split statements.
      * <p>
-     *     The default splitting regex is ";".
+     * The default splitting regex is ";".
      * </p>
      * <p>
-     *     To change this use {@link #split(String, String)}.
+     * To change this use {@link #split(String, String)}.
      * </p>
      */
-    public static final String DEFAULT_SPLIT_REGEX = ";";
+    public static final String DEFAULT_SPLIT_MARKER = ";";
+
     /**
      * This is the default delimiter keyword, which is regularly used to change the delimiter.
      * <p>
-     *     The default delimiter keyword is "DELIMITER".
+     * The default delimiter keyword is "DELIMITER".
      * </p>
      * <p>
-     *     To change this use {@link #split(String, String, String)}.
+     * To change this use {@link #split(String, String, String)}.
      * </p>
      */
     public static final String DEFAULT_DELIMITER_KEYWORD = "DELIMITER";
 
+    private static final Pattern CLEANER = Pattern.compile("^([ \n\r]+)");
+    @RegExp
+    private static final String DELIMITER_FORMAT = "^%s (?<delimiter>[^ \n\r]+)";
+
     /**
      * Splits a string of SQL statements into an array of statements.
      * <p>
-     *     This method is used to split a string of SQL statements into an array of statements.
-     *     The splitting is done by the regex, which is defined in the second argument of the method.
-     *     The delimiter is defined in the third argument of the method.
+     * The splitting is done by the regex, which default is ({@link #DEFAULT_SPLIT_MARKER}).
+     * The default delimiter keyword is ({@link #DEFAULT_DELIMITER_KEYWORD}).
+     * The delimiter is not case sensitive.
      * </p>
-     * @param statements the statements to split
-     * @param splitRegex the regex used to split the statements
-     * @param delimiter the delimiter used to change the delimiter in the statements
+     *
+     * @param sql         the statements to split
+     * @param splitMarker the marker used to split the statements
+     * @param delimiter   the delimiter keyword to change the delimiter in the statements
      * @return the array of statements
      */
-    public static String[] split(String statements, String splitRegex, String delimiter) {
+    public static String[] split(String sql, String splitMarker, String delimiter) {
+        // Pattern to extract new delimiter
+        var delimiterPattern = Pattern.compile(DELIMITER_FORMAT.formatted(delimiter), Pattern.CASE_INSENSITIVE);
+        // Pattern to split statements
+        var splitter = Pattern.compile(splitMarker);
+        // The remaining statements that are not yet parsed
+        var remaining = sql;
+        List<String> statements = new ArrayList<>();
+        while (!remaining.isBlank()) {
 
-        int index = 0;
-
-        ArrayList<String> result = new ArrayList<>();
-
-        String currentSplitRegex = splitRegex;
-
-        while (index <= statements.length()) {
-
-            int nextDelimiterIndex = indexOfIgnoreCase(statements.substring(index), delimiter);
-            int nextCurrentSplitRegexIndex = indexOfIgnoreCase(statements.substring(index), currentSplitRegex);
-            int nextSplitRegexIndex = indexOfIgnoreCase(statements.substring(index), splitRegex);
-
-            if (nextDelimiterIndex == -1 && nextSplitRegexIndex == -1) {
-                break;
-            }
-
-            //as long as we have a different delimiter than the default one, we just add a delimiter statement to each statement
-            if (!currentSplitRegex.equals(splitRegex) &&
-                    nextCurrentSplitRegexIndex >= 0 &&
-                    nextCurrentSplitRegexIndex <= nextDelimiterIndex) {
-                int statementLength = indexOfIgnoreCase(statements.substring(index), currentSplitRegex);
-                statementLength += currentSplitRegex.length();
-
-                String currentStatement = statements.substring(index, index + statementLength);
-
-                currentStatement = delimiter + " " + currentSplitRegex + "\n" + currentStatement;
-                currentStatement += "\n" + delimiter + " " + splitRegex;
-
-                result.add(currentStatement);
-
-                index += statementLength;
-
+            remaining = CLEANER.matcher(remaining).replaceAll("");
+            if (remaining.isBlank()) break;
+            Matcher delimiterCheck = delimiterPattern.matcher(remaining);
+            // Check if next statement is internal delimiter
+            if (delimiterCheck.find()) {
+                // Extract delimiter
+                splitter = Pattern.compile(Pattern.quote(delimiterCheck.group("delimiter")));
+                // Remove the delimiter statement
+                remaining = delimiterCheck.reset().replaceAll("");
                 continue;
             }
 
-            if (nextSplitRegexIndex == -1) {
-                String newDelimiter = parseNewDelimiter(statements.substring(index + nextDelimiterIndex), delimiter);
-                if (!newDelimiter.equals(splitRegex)) {
-                    currentSplitRegex = newDelimiter;
-                } else {
-                    currentSplitRegex = splitRegex;
-                }
-                int statementLength = indexOfIgnoreCase(statements.substring(index), currentSplitRegex);
-                statementLength += currentSplitRegex.length();
-                index += statementLength;
-                continue;
-            } else if (nextDelimiterIndex != -1 &&nextDelimiterIndex < nextSplitRegexIndex) {
-                if (isWhitespaceOnly(statements.substring(index, index + nextDelimiterIndex))) {
-                    String newDelimiter = parseNewDelimiter(statements.substring(index + nextDelimiterIndex), delimiter);
-                    if (!newDelimiter.equals(splitRegex)) {
-                        currentSplitRegex = newDelimiter;
-                    } else {
-                        currentSplitRegex = splitRegex;
-                    }
-                    int statementLength = indexOfIgnoreCase(statements.substring(index), currentSplitRegex);
-                    statementLength += currentSplitRegex.length();
-                    index += statementLength;
-                    continue;
-                }
-            }
+            var split = splitter.split(remaining, 2);
+            // Replace the remaining statements
+            remaining = split.length == 2 ? split[1] : "";
 
-            int statementLength = indexOfIgnoreCase(statements.substring(index), splitRegex);
-            statementLength += splitRegex.length();
-
-            String currentStatement = statements.substring(index, index + statementLength);
-
-            result.add(currentStatement);
-
-            index += statementLength;
-
+            // Store current frame with correct sql delimiter
+            statements.add(split[0] + ";");
         }
 
-        return result.toArray(new String[0]);
-
+        return statements.toArray(new String[0]);
     }
 
     /**
      * Splits a string of SQL statements into an array of statements.
      * <p>
-     *     The splitting is done by the regex, which is defined in the second argument of the method.
-     *     The default delimiter keyword is "DELIMITER" ({@link #DEFAULT_DELIMITER_KEYWORD}).
-     *     The delimiter is not case insensitive.
+     * The splitting is done by the marker
+     * The default delimiter keyword is {@link #DEFAULT_DELIMITER_KEYWORD}.
+     * The delimiter is not case sensitive.
      * </p>
-     * @param statements the statements to split
-     * @param splitRegex the regex used to split the statements
+     *
+     * @param sql         the statements to split
+     * @param splitMarker the marker used to split the statements
      * @return the array of statements
      */
-    public static String[] split(String statements, String splitRegex) {
-        return split(statements, splitRegex, DEFAULT_DELIMITER_KEYWORD);
+    public static String[] split(String sql, String splitMarker) {
+        return split(sql, splitMarker, DEFAULT_DELIMITER_KEYWORD);
     }
 
     /**
      * Splits a string of SQL statements into an array of statements.
      * <p>
-     *     The splitting is done by the regex, which default is ";" ({@link #DEFAULT_SPLIT_REGEX}).
-     *     The default delimiter keyword is "DELIMITER" ({@link #DEFAULT_DELIMITER_KEYWORD}).
-     *     The delimiter is not case insensitive.
+     * The splitting is done by the regex, which default is ({@link #DEFAULT_SPLIT_MARKER}).
+     * The default delimiter keyword is ({@link #DEFAULT_DELIMITER_KEYWORD}).
+     * The delimiter is not case sensitive.
      * </p>
-     * @param statements the statements to split
+     *
+     * @param sql the statements to split
      * @return the array of statements
      */
-    public static String[] split(String statements) {
-        return split(statements, DEFAULT_SPLIT_REGEX, DEFAULT_DELIMITER_KEYWORD);
+    public static String[] split(String sql) {
+        return split(sql, DEFAULT_SPLIT_MARKER, DEFAULT_DELIMITER_KEYWORD);
     }
-
-    private static String parseNewDelimiter(String string, String delimiter) {
-
-        for (String currentString : string.split("(?<=\\S)\\s")) {
-            if (currentString.equalsIgnoreCase(delimiter)) {
-                continue;
-            } else if (currentString.equalsIgnoreCase("")) {
-                continue;
-            }
-            return currentString;
-        }
-
-        return "";
-
-    }
-
-    private static int indexOfIgnoreCase(String string, String substring) {
-        return string.toLowerCase().indexOf(substring.toLowerCase());
-    }
-
-    private static boolean isWhitespaceOnly(String str) {
-        return str != null && str.matches("^\\s*$");
-    }
-
 }
--- sadu-mariadb/src/test/java/de/chojo/sadu/databases/StatementSplitterTest.java
+++ sadu-mariadb/src/test/java/de/chojo/sadu/databases/StatementSplitterTest.java
@@ -15,7 +15,7 @@ public class StatementSplitterTest {
     @Test
     void testSimpleSplit() {
 
-        final String SQL = "SELECT * FROM table; SELECT * FROM table2;";
+        String SQL = "SELECT * FROM table; SELECT * FROM table2;";
 
         String[] splitStatements = StatementSplitter.split(SQL);
 
@@ -28,13 +28,13 @@ public class StatementSplitterTest {
     @Test
     void testDelimiterSplit() {
 
-        final String SQL = "SELECT * FROM table; DELIMITER $$ SELECT * FROM table2 $$ DELIMITER ; SELECT * FROM table3;";
+        String SQL = "SELECT * FROM table; DELIMITER $$ SELECT * FROM table2 $$ DELIMITER ; SELECT * FROM table3;";
 
         String [] splitStatements = StatementSplitter.split(SQL);
 
         assertEquals(3, splitStatements.length);
         assertEquals("SELECT * FROM table;", splitStatements[0].trim());
-        assertEquals("DELIMITER $$ SELECT * FROM table2 $$ DELIMITER ;", splitStatements[1].replaceAll("\\s+", " "));
+        assertEquals("SELECT * FROM table2 ;", splitStatements[1].replaceAll("\\s+", " "));
         assertEquals("SELECT * FROM table3;", splitStatements[2].trim());
 
     }
@@ -42,25 +42,23 @@ public class StatementSplitterTest {
     @Test
     void testSplitOnStatementsWithoutSpaces() {
 
-        final String SQL = "SELECT * FROM table;SELECT test FROM table;DELIMITER $$ SELECT * FROM table2 $$INSERT " +
+        String SQL = "SELECT * FROM table;SELECT test FROM table;DELIMITER $$ SELECT * FROM table2 $$INSERT " +
                 "INTO table VALUES(x,y)$$DELIMITER ; SELECT * FROM table3;";
 
         String[] splitStatements = StatementSplitter.split(SQL);
 
-        assertEquals(7, splitStatements.length);
+        assertEquals(5, splitStatements.length);
         assertEquals("SELECT * FROM table;", splitStatements[0].trim());
         assertEquals("SELECT test FROM table;", splitStatements[1].trim());
-        assertEquals("DELIMITER $$", splitStatements[2].trim());
-        assertEquals("SELECT * FROM table2 $$", splitStatements[3].trim());
-        assertEquals("INSERT INTO table VALUES(x,y)$$", splitStatements[4].trim());
-        assertEquals("DELIMITER ;", splitStatements[5].trim());
-        assertEquals("SELECT * FROM table3;", splitStatements[6].trim());
+        assertEquals("SELECT * FROM table2 ;", splitStatements[2].trim());
+        assertEquals("INSERT INTO table VALUES(x,y);", splitStatements[3].trim());
+        assertEquals("SELECT * FROM table3;", splitStatements[4].trim());
 
     }
 
     @Test
     void testSplitOnTextBlock() {
-        final String sql = """
+        String sql = """
                 DELIMITER //
                 CREATE FUNCTION IF NOT EXISTS is_prime (number INT) RETURNS BOOLEAN
                 BEGIN
@@ -93,26 +91,26 @@ public class StatementSplitterTest {
         String[] splitStatements = StatementSplitter.split(sql);
 
         assertEquals(2, splitStatements.length);
-        assertEquals("DELIMITER // CREATE FUNCTION IF NOT EXISTS is_prime (number INT) RETURNS BOOLEAN BEGIN DECLARE i" +
+        assertEquals("CREATE FUNCTION IF NOT EXISTS is_prime (number INT) RETURNS BOOLEAN BEGIN DECLARE i" +
                 " INT DEFAULT 2; find_prime: WHILE i < number DO IF number % i = 0 THEN RETURN FALSE; END IF; SET i" +
-                " = i + 1; END WHILE find_prime; RETURN TRUE; END // DELIMITER ;",
+                " = i + 1; END WHILE find_prime; RETURN TRUE; END ;",
                 splitStatements[0].trim().replaceAll(" +", " ")
                 .replaceAll("\\s+", " "));
-        assertEquals("DELIMITER // CREATE FUNCTION IF NOT EXISTS next_prime_number(number INT) RETURNS INT BEGIN DECLARE" +
+        assertEquals("CREATE FUNCTION IF NOT EXISTS next_prime_number(number INT) RETURNS INT BEGIN DECLARE" +
                 " isPrime BOOLEAN DEFAULT FALSE; DECLARE currentNumber INT DEFAULT number; looping: WHILE isPrime =" +
                 " FALSE DO SET currentNumber = currentNumber + 1; IF is_prime(currentNumber) = TRUE THEN RETURN " +
-                "currentNumber; END IF; END WHILE looping; END // DELIMITER ;",
-                splitStatements[4].trim().replaceAll(" +", " ")
+                "currentNumber; END IF; END WHILE looping; END ;",
+                splitStatements[1].trim().replaceAll(" +", " ")
                         .replaceAll("\\s+", " "));
 
     }
 
     @Test
     void testSplitWithDelimiterString() {
-        final String sql = """
+        String sql = """
                 SELECT * FROM delimiter;
                 DELIMITER //
-                SELECT test FROM table2 //
+                SELECT delimiter FROM table2 //
                 SELECT test FROM table //
                 DELIMITER ;
                 SELECT * FROM 'delimiter';
@@ -123,9 +121,9 @@ public class StatementSplitterTest {
         assertEquals(4, splitStatements.length);
         assertEquals("SELECT * FROM delimiter;", splitStatements[0].trim().replaceAll(" +", " ")
                 .replaceAll("\\s+", " "));
-        assertEquals("DELIMITER // SELECT delimiter FROM table2 // DELIMITER ;", splitStatements[2].trim().replaceAll(" +", " ")
+        assertEquals("SELECT delimiter FROM table2 ;", splitStatements[1].trim().replaceAll(" +", " ")
                 .replaceAll("\\s+", " "));
-        assertEquals("DELIMITER // SELECT test FROM table // DELIMITER ;", splitStatements[2].trim().replaceAll(" +", " ")
+        assertEquals("SELECT test FROM table ;", splitStatements[2].trim().replaceAll(" +", " ")
                 .replaceAll("\\s+", " "));
         assertEquals("SELECT * FROM 'delimiter';", splitStatements[3].trim().replaceAll(" +", " ")
                 .replaceAll("\\s+", " "));
