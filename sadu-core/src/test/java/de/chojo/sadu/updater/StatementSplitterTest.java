7b63b9303acfaf73a1c7c34560ac4247ab205007
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2023-11-21 23:22:37 +0100

    Apply formatting

--- sadu-core/src/test/java/de/chojo/sadu/updater/StatementSplitterTest.java
+++ sadu-core/src/test/java/de/chojo/sadu/updater/StatementSplitterTest.java
@@ -6,7 +6,6 @@
 
 package de.chojo.sadu.updater;
 
-import de.chojo.sadu.updater.StatementSplitter;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,7 +14,6 @@ public class StatementSplitterTest {
 
     @Test
     void testSimpleSplit() {
-
         String SQL = "SELECT * FROM table; SELECT * FROM table2;";
 
         String[] splitStatements = StatementSplitter.split(SQL);
@@ -23,28 +21,24 @@ public class StatementSplitterTest {
         assertEquals(2, splitStatements.length);
         assertEquals("SELECT * FROM table;", splitStatements[0].trim());
         assertEquals("SELECT * FROM table2;", splitStatements[1].trim());
-
     }
 
     @Test
     void testDelimiterSplit() {
-
         String SQL = "SELECT * FROM table; DELIMITER $$ SELECT * FROM table2 $$ DELIMITER ; SELECT * FROM table3;";
 
-        String [] splitStatements = StatementSplitter.split(SQL);
+        String[] splitStatements = StatementSplitter.split(SQL);
 
         assertEquals(3, splitStatements.length);
         assertEquals("SELECT * FROM table;", splitStatements[0].trim());
         assertEquals("SELECT * FROM table2 ;", splitStatements[1].replaceAll("\\s+", " "));
         assertEquals("SELECT * FROM table3;", splitStatements[2].trim());
-
     }
 
     @Test
     void testSplitOnStatementsWithoutSpaces() {
-
         String SQL = "SELECT * FROM table;SELECT test FROM table;DELIMITER $$ SELECT * FROM table2 $$INSERT " +
-                "INTO table VALUES(x,y)$$DELIMITER ; SELECT * FROM table3;";
+                     "INTO table VALUES(x,y)$$DELIMITER ; SELECT * FROM table3;";
 
         String[] splitStatements = StatementSplitter.split(SQL);
 
@@ -54,7 +48,6 @@ public class StatementSplitterTest {
         assertEquals("SELECT * FROM table2 ;", splitStatements[2].trim());
         assertEquals("INSERT INTO table VALUES(x,y);", splitStatements[3].trim());
         assertEquals("SELECT * FROM table3;", splitStatements[4].trim());
-
     }
 
     @Test
@@ -93,17 +86,16 @@ public class StatementSplitterTest {
 
         assertEquals(2, splitStatements.length);
         assertEquals("CREATE FUNCTION IF NOT EXISTS is_prime (number INT) RETURNS BOOLEAN BEGIN DECLARE i" +
-                " INT DEFAULT 2; find_prime: WHILE i < number DO IF number % i = 0 THEN RETURN FALSE; END IF; SET i" +
-                " = i + 1; END WHILE find_prime; RETURN TRUE; END ;",
+                     " INT DEFAULT 2; find_prime: WHILE i < number DO IF number % i = 0 THEN RETURN FALSE; END IF; SET i" +
+                     " = i + 1; END WHILE find_prime; RETURN TRUE; END ;",
                 splitStatements[0].trim().replaceAll(" +", " ")
-                .replaceAll("\\s+", " "));
+                        .replaceAll("\\s+", " "));
         assertEquals("CREATE FUNCTION IF NOT EXISTS next_prime_number(number INT) RETURNS INT BEGIN DECLARE" +
-                " isPrime BOOLEAN DEFAULT FALSE; DECLARE currentNumber INT DEFAULT number; looping: WHILE isPrime =" +
-                " FALSE DO SET currentNumber = currentNumber + 1; IF is_prime(currentNumber) = TRUE THEN RETURN " +
-                "currentNumber; END IF; END WHILE looping; END ;",
+                     " isPrime BOOLEAN DEFAULT FALSE; DECLARE currentNumber INT DEFAULT number; looping: WHILE isPrime =" +
+                     " FALSE DO SET currentNumber = currentNumber + 1; IF is_prime(currentNumber) = TRUE THEN RETURN " +
+                     "currentNumber; END IF; END WHILE looping; END ;",
                 splitStatements[1].trim().replaceAll(" +", " ")
                         .replaceAll("\\s+", " "));
-
     }
 
     @Test
