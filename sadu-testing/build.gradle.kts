228d1b11c21c5d724ad2ca1c2d2271a5b05f752f
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2023-11-21 23:43:55 +0100

    Add description to testing module

--- sadu-testing/build.gradle.kts
+++ sadu-testing/build.gradle.kts
@@ -1,3 +1,5 @@
+description = "SADU module for checking validity of updater files"
+
 dependencies {
 
     api(platform("org.junit:junit-bom:5.10.1"))
@@ -5,8 +7,4 @@ dependencies {
     api(project(":sadu-core"))
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
