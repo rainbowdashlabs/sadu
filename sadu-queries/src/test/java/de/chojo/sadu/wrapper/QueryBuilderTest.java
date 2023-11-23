f1d6a725ab53950e34d5ad3d1e9dae27468d31af
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2023-11-23 17:43:03 +0100

    Switch to mariadb container for keys test

--- sadu-queries/src/test/java/de/chojo/sadu/wrapper/QueryBuilderTest.java
+++ sadu-queries/src/test/java/de/chojo/sadu/wrapper/QueryBuilderTest.java
@@ -7,58 +7,67 @@
 package de.chojo.sadu.wrapper;
 
 import com.zaxxer.hikari.HikariDataSource;
-import de.chojo.sadu.databases.SqLite;
+import de.chojo.sadu.databases.MariaDb;
 import de.chojo.sadu.datasource.DataSourceCreator;
+import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 
+import static de.chojo.sadu.MariaDbDatabase.createContainer;
+
 class QueryBuilderTest {
+    private static GenericContainer<?> db;
     private HikariDataSource source;
+    @Language("mariadb")
+    private static final String CREATE_TABLE = """
+            CREATE TABLE IF NOT EXISTS test(
+                id   INTEGER PRIMARY KEY AUTO_INCREMENT,
+                user_name TEXT   NOT NULL
+                )
+                """;
+    @Language("mariadb")
+    private static final String INSERT_SINGLE = "INSERT INTO test(user_name) VALUES(?)";
+    @Language("mariadb")
+    private static final String INSERT_MULTI = "INSERT INTO test(user_name) VALUES(?),(?),(?)";
 
     @BeforeAll
     public static void setup() {
+        db = createContainer("root", "root");
         QueryBuilderConfig.setDefault(QueryBuilderConfig.builder().throwExceptions().build());
     }
 
     @BeforeEach
     public void setupDB() {
-        source = DataSourceCreator.create(SqLite.sqlite())
-                .configure(config -> config.path("data.db"))
+        source = DataSourceCreator.create(MariaDb.get())
+                .configure(config -> config.host(db.getHost()).port(db.getFirstMappedPort()).database("mysql"))
                 .create()
+                .usingUsername("root")
+                .usingPassword("root")
                 .build();
     }
 
     @AfterEach
     public void after() throws IOException {
-        Files.delete(Path.of("data.db"));
         source.close();
     }
 
     @Test
     public void getKey() {
         QueryBuilder.builder(source).defaultConfig()
-                .queryWithoutParams(
-                        """
-                                CREATE TABLE IF NOT EXISTS test(
-                                    id   INTEGER PRIMARY KEY AUTOINCREMENT,
-                                    user_name TEXT   NOT NULL
-                                    )
-                                    """)
+                .queryWithoutParams(CREATE_TABLE)
                 .update()
                 .send()
                 .join();
 
         long l = QueryBuilder.builder(source).defaultConfig()
-                .query("INSERT INTO test(user_name) VALUES(?)")
+                .query(INSERT_SINGLE)
                 .parameter(stmt -> stmt.setString("test"))
                 .insert()
                 .keySync()
@@ -68,28 +77,21 @@ class QueryBuilderTest {
     }
 
     @Test
-    @Disabled
     public void getKeys() {
         QueryBuilder.builder(source).defaultConfig()
-                .queryWithoutParams(
-                        """
-                                CREATE TABLE IF NOT EXISTS test(
-                                    id   INTEGER PRIMARY KEY AUTOINCREMENT,
-                                    user_name TEXT   NOT NULL
-                                    )
-                                    """)
+                .queryWithoutParams(CREATE_TABLE)
                 .update()
                 .send()
                 .join();
 
         List<Long> l = QueryBuilder.builder(source).defaultConfig()
-                .query("INSERT INTO test(user_name) VALUES(?),(?),(?)")
+                .query(INSERT_MULTI)
                 .parameter(stmt -> stmt.setString("test").setString("test").setString("test"))
                 .insert()
                 .keysSync();
 
-        // For some reason sqlite returns only the last id instead of all three.
-        Assertions.assertEquals(3, l.size());
+        // Only the last key is returned. Multiple keys would be returned if there would be multiple keys in a single row.
+        Assertions.assertEquals(1, l.size());
     }
 
 }
