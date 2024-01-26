53357460065da4074b1a5ccd650d15260d6d0ce8
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-01-26 16:15:18 +0100

    Add sadu-mapper as a dependency to various projects
    The sadu-mapper project was added to the build scripts of sadu-mariadb, sadu-mysql, and sadu-sqlite. This addition will enable these projects to handle more complex mapping scenarios efficiently.

--- sadu-mysql/build.gradle.kts
+++ sadu-mysql/build.gradle.kts
@@ -2,6 +2,7 @@ description = "SADU module for interaction with a MySQL database"
 
 dependencies {
     api(project(":sadu-updater"))
+    api(project(":sadu-mapper"))
     testImplementation(project(":sadu-datasource"))
     testImplementation(testlibs.bundles.database.mysql)
     testImplementation(testlibs.slf4j.noop)
