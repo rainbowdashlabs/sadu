033e8784c6e458e9989c8aa39b2babde67224389
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-01-26 12:39:46 +0100

    Implement RowMapping interface in RowMapper class
    In the RowMapper class, the RowMapping interface is now implemented. A 'map' method, which maps rows and handles SQL exceptions, has been added to improve the robustness of the application and the management of potential errors. The refactoring also aided in maintaining clean and efficient code structure.

---
+++ sadu-mapper/src/main/java/de/chojo/sadu/mapper/rowmapper/RowMapping.java
@@ -0,0 +1,14 @@
+package de.chojo.sadu.mapper.rowmapper;
+
+import de.chojo.sadu.exceptions.ThrowingFunction;
+import de.chojo.sadu.wrapper.util.Row;
+
+import java.sql.SQLException;
+
+public interface RowMapping<T> {
+    T map(Row row) throws SQLException;
+
+    static <V> RowMapping<V> create(ThrowingFunction<V, Row, SQLException> mapper){
+        return mapper::apply;
+    }
+}
