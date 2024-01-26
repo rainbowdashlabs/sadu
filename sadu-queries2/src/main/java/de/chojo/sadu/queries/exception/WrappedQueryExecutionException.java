80d16f277fe44f9f0d8a0234c904e18eb2a3834a
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-01-26 12:50:21 +0100

    Add new exceptions for query execution
    Two new exception classes, 'WrappedQueryExecutionException' and 'QueryExecutionException', were added in the 'de.chojo.sadu.queries.exception' package. These exceptions are designed to handle issues that may arise during the execution of queries. They wrap typical SQL exceptions as runtime exceptions, which simplifies error handling.

---
+++ sadu-queries2/src/main/java/de/chojo/sadu/queries/exception/WrappedQueryExecutionException.java
@@ -0,0 +1,62 @@
+/*
+ *     SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ *     Copyright (C) RainbowDashLabs and Contributor
+ */
+
+package de.chojo.sadu.queries.exception;
+
+import java.sql.SQLException;
+
+/**
+ * Exception to wrap {@link Exception} as {@link RuntimeException} thrown during queries executed by {@link de.chojo.sadu.queries.stages.Query}
+ */
+public class WrappedQueryExecutionException extends RuntimeException {
+    /**
+     * Cause of the exception
+     */
+    private SQLException cause;
+
+    /**
+     * Creates a new exception.
+     *
+     * @param message message
+     */
+    public WrappedQueryExecutionException(String message) {
+        super(message);
+    }
+
+    /**
+     * @throws ClassCastException If the throwable is not a {@link SQLException}
+     */
+    @Override
+    public synchronized Throwable initCause(Throwable cause) {
+        this.cause = (SQLException) cause;
+        return super.initCause(cause);
+    }
+
+
+    /**
+     * SQL state of the exception
+     *
+     * @return sql state
+     */
+    public String getSQLState() {
+        return cause.getSQLState();
+    }
+
+    /**
+     * Error code of the exception
+     *
+     * @return error code
+     */
+    public int getErrorCode() {
+        return cause.getErrorCode();
+    }
+
+    @Override
+    public String getMessage() {
+        return cause.getMessage();
+    }
+
+}
