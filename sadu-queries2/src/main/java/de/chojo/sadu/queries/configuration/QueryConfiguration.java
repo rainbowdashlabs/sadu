b0c289898da7ff4fa527b6e7c20e0954582805f0
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-01-29 17:53:03 +0100

    Add Javadocs to methods in QueryConfiguration
    Javadocs have been added to most methods in the QueryConfiguration class, improving code readability and clarity of method purposes. Each JavaDoc provides a brief explanation of the method's functionality, its parameters, and what it returns.

--- sadu-queries2/src/main/java/de/chojo/sadu/queries/configuration/QueryConfiguration.java
+++ sadu-queries2/src/main/java/de/chojo/sadu/queries/configuration/QueryConfiguration.java
@@ -47,18 +47,50 @@ public class QueryConfiguration {
         this.rowMapperRegistry = rowMapperRegistry;
     }
 
+    /**
+     * Retrieves the default QueryConfiguration.
+     *
+     * @return the default QueryConfiguration
+     */
     public static QueryConfiguration getDefault() {
         return DEFAULT.get();
     }
 
+    /**
+     * Sets the default query configuration.
+     *
+     * @param configuration the query configuration to set as default
+     */
     public static void setDefault(QueryConfiguration configuration) {
         DEFAULT.set(configuration);
     }
 
+    /**
+     * Creates a new QueryConfigurationBuilder instance with the given DataSource.
+     *
+     * @param source the DataSource to use for QueryConfiguration
+     * @return a QueryConfigurationBuilder instance
+     */
+    public static QueryConfigurationBuilder builder(DataSource source) {
+        return new QueryConfigurationBuilder(source);
+    }
+
+    /**
+     * Returns a new QueryConfiguration object with the provided query and other configuration settings.
+     *
+     * @param query the query to be associated with the configuration
+     * @return the new QueryConfiguration object
+     */
     public QueryConfiguration forQuery(QueryImpl query) {
         return new QueryConfiguration(query, dataSource, atomic, throwExceptions, exceptionHandler, rowMapperRegistry);
     }
 
+    /**
+     * Handles a SQLException by invoking the exceptionHandler consumer, logging the exception,
+     * and potentially throwing a WrappedQueryExecutionException.
+     *
+     * @param e the SQLException to handle
+     */
     public void handleException(SQLException e) {
         exceptionHandler.accept(e);
         query.logException(e);
@@ -67,34 +99,79 @@ public class QueryConfiguration {
         }
     }
 
+    /**
+     * Retrieves the value of the atomic flag.
+     *
+     * @return {@code true} if atomic flag is set; {@code false} otherwise.
+     */
     public boolean atomic() {
         return atomic;
     }
 
+    /**
+     * Retrieves the value of the throwExceptions field.
+     *
+     * @return the value of the throwExceptions field
+     */
     public boolean throwExceptions() {
         return throwExceptions;
     }
 
+    /**
+     * Retrieves the {@link RowMapperRegistry} object from the {@link QueryConfiguration}.
+     *
+     * @return The {@link RowMapperRegistry} object from the {@link QueryConfiguration}.
+     */
     public RowMapperRegistry rowMapperRegistry() {
         return rowMapperRegistry;
     }
 
+    /**
+     * Returns the DataSource object associated with this QueryConfiguration.
+     *
+     * @return the DataSource object
+     */
     public DataSource dataSource() {
         return dataSource;
     }
 
+    /**
+     * Throws an IllegalStateException with a specific error message.
+     * This method is meant to be called when the QueryConfiguration doesn't hold a configuration.
+     * To create a connection, use the {@link #withSingleTransaction} method and make sure to use try-with-resources on the new configuration instance.
+     *
+     * @throws IllegalStateException if the QueryConfiguration doesn't hold a configuration
+     */
     public Connection connection() {
         throw new IllegalStateException("Query configuration doesnt hold a configuration.\nCall #withSingleTransaction for a connection to be created and use try with resources on the new configuration instance");
     }
 
+    /**
+     * Checks if the current query configuration has an active connection.
+     *
+     * @return {@code true} if there is an active connection, {@code false} otherwise.
+     */
     public boolean hasConnection() {
         return false;
     }
 
+    /**
+     * Executes a SQL query with the given SQL statement and format arguments.
+     *
+     * @param sql    the SQL statement to be executed
+     * @param format the format arguments to be applied to the SQL statement
+     * @return a parsed query ready for execution
+     */
     public ParsedQuery query(@Language("sql") String sql, Object... format) {
         return Query.query(this, sql, format);
     }
 
+    /**
+     * Returns a new instance of the ConnectedQueryConfiguration class with the "single transaction" configuration applied.
+     * This means that a single transaction will be used for the queries executed using this configuration.
+     *
+     * @return A new instance of the ConnectedQueryConfiguration class with the "single transaction" configuration applied.
+     */
     public ConnectedQueryConfiguration withSingleTransaction() {
         return new ConnectedQueryConfiguration(query, dataSource, true, throwExceptions, exceptionHandler, rowMapperRegistry);
     }
