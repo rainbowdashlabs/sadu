3abd3da32877bc88ebf1a2ca9ddd399a642049a3
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-01-30 13:30:16 +0100

    Update module-info.java in sadu-queries2
    Reorganized the definitions for sadu-queries2 in module-info.java. Exports for new query packages have been added further segregating the code by responsibility thereby improving code organization and maintaining clarity in defining API and implementing details.

--- sadu-queries2/src/main/java/module-info.java
+++ sadu-queries2/src/main/java/module-info.java
@@ -3,14 +3,17 @@ module sadu.sadu.queries2.main {
     requires transitive sadu.sadu.core.main;
     requires transitive sadu.sadu.mapper.main;
 
+    exports de.chojo.sadu.queries.api.base;
     exports de.chojo.sadu.queries.api.call;
     exports de.chojo.sadu.queries.api.call.adapter;
     exports de.chojo.sadu.queries.api.call.calls;
     exports de.chojo.sadu.queries.api.execution.reading;
     exports de.chojo.sadu.queries.api.execution.writing;
+    exports de.chojo.sadu.queries.api.parameter;
     exports de.chojo.sadu.queries.api.query;
     exports de.chojo.sadu.queries.api.results.reading;
     exports de.chojo.sadu.queries.api.results.writing;
+    exports de.chojo.sadu.queries.api.storage;
 
     exports de.chojo.sadu.queries.call;
     exports de.chojo.sadu.queries.call.adapter;
@@ -21,13 +24,10 @@ module sadu.sadu.queries2.main {
 
     exports de.chojo.sadu.queries.exception;
 
-    exports de.chojo.sadu.queries.params;
+    exports de.chojo.sadu.queries.parameter;
 
     exports de.chojo.sadu.queries.query;
 
-    exports de.chojo.sadu.queries.stages;
-    exports de.chojo.sadu.queries.stages.base;
-    exports de.chojo.sadu.queries.stages.calls;
     exports de.chojo.sadu.queries.stages.execution.reading;
     exports de.chojo.sadu.queries.stages.execution.writing;
     exports de.chojo.sadu.queries.stages.results.reading;
