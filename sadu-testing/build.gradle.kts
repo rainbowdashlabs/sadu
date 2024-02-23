de11f9fd5a313cae8def8fb0c33a3ccf18cabaa3
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-02-23 16:02:47 +0100

    Remove dynamic version for junit

--- sadu-testing/build.gradle.kts
+++ sadu-testing/build.gradle.kts
@@ -2,7 +2,7 @@ description = "SADU module for checking validity of updater files"
 
 dependencies {
 
-    api(platform("org.junit:junit-bom:5+"))
+    api(platform("org.junit:junit-bom:5.10.2"))
     api("org.junit.jupiter:junit-jupiter")
     api(project(":sadu-core"))
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
