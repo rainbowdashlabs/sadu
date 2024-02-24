7e46f6546571e422b7f0af8c0d10166e7b5f9e09
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-02-24 11:49:39 +0100

    Add InsertionBatchResultImpl and InsertionResultImpl classes
    Two new classes named `InsertionBatchResultImpl` and `InsertionResultImpl` have been added to the 'insertion' sub-package of 'sadu-queries'. These classes extend `ManipulationBatchResultImpl` and `ManipulationResultImpl` respectively, providing functionality for managing batch insertion results, including extraction of keys. These additions enhance the handling of batch insertion results in the project.

---
+++ sadu-queries/src/main/java/de/chojo/sadu/queries/results/writing/insertion/InsertionResultImpl.java
@@ -0,0 +1,26 @@
+package de.chojo.sadu.queries.results.writing.insertion;
+
+import de.chojo.sadu.queries.api.base.QueryProvider;
+import de.chojo.sadu.queries.api.results.writing.insertion.InsertionResult;
+import de.chojo.sadu.queries.results.writing.manipulation.ManipulationResultImpl;
+
+import java.util.Collections;
+import java.util.List;
+
+public class InsertionResultImpl extends ManipulationResultImpl implements InsertionResult {
+    private final List<Long> keys;
+
+    public InsertionResultImpl(QueryProvider query, int rows, List<Long> keys) {
+        super(query, rows);
+        this.keys = keys;
+    }
+
+    public static InsertionResultImpl empty(QueryProvider query) {
+        return new InsertionResultImpl(query, 0, Collections.emptyList());
+    }
+
+    @Override
+    public List<Long> keys() {
+        return keys;
+    }
+}
