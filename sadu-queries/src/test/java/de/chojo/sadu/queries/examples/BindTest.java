f4972dd42f7411b4b80421fbcad218e3085a19ac
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-10-21 12:47:31 +0200

    Remove unused imports

--- sadu-queries/src/test/java/de/chojo/sadu/queries/examples/BindTest.java
+++ sadu-queries/src/test/java/de/chojo/sadu/queries/examples/BindTest.java
@@ -10,11 +10,7 @@ import de.chojo.sadu.PostgresDatabase;
 import de.chojo.sadu.mapper.RowMapperRegistry;
 import de.chojo.sadu.mapper.rowmapper.RowMapper;
 import de.chojo.sadu.postgresql.mapper.PostgresqlMapper;
-import de.chojo.sadu.queries.api.call.Call;
-import de.chojo.sadu.queries.api.call.calls.Calls;
 import de.chojo.sadu.queries.api.configuration.QueryConfiguration;
-import de.chojo.sadu.queries.api.results.reading.Result;
-import de.chojo.sadu.queries.api.results.writing.manipulation.ManipulationResult;
 import de.chojo.sadu.queries.configuration.QueryConfigurationBuilder;
 import de.chojo.sadu.queries.examples.dao.User;
 import org.junit.jupiter.api.AfterEach;
@@ -24,14 +20,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 import static de.chojo.sadu.PostgresDatabase.createContainer;
 import static de.chojo.sadu.queries.api.call.Call.call;
-import static de.chojo.sadu.queries.call.adapter.UUIDAdapter.AS_STRING;
 
 @SuppressWarnings({"unused", "ResultOfMethodCallIgnored", "OptionalGetWithoutIsPresent", "RedundantExplicitVariableType"})
 public class BindTest {
@@ -44,7 +36,7 @@ public class BindTest {
     void before() throws IOException, SQLException {
         db = createContainer("postgres", "postgres");
         query = new QueryConfigurationBuilder(db.dataSource()).setRowMapperRegistry(new RowMapperRegistry().register(PostgresqlMapper.getDefaultMapper())
-                .register(RowMapper.forClass(User.class).mapper(User.map()).build())).build();
+                                                                                                           .register(RowMapper.forClass(User.class).mapper(User.map()).build())).build();
     }
 
     @AfterEach
@@ -56,10 +48,10 @@ public class BindTest {
     @Test
     public void failOnInvalidToken() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-        List<User> users = query.query("SELECT * FROM users WHERE id = ? AND name ILIKE :name1")
-                .single(call().bind(1).bind("name1", "lilly"))
-                .map(User.map())
-                .all();
+            List<User> users = query.query("SELECT * FROM users WHERE id = ? AND name ILIKE :name1")
+                                    .single(call().bind(1).bind("name1", "lilly"))
+                                    .map(User.map())
+                                    .all();
         });
     }
 }
